985e62ba2a807498cf7763b2ee2449d1955e6e13
Daniel Chiu <daniel@chiubaka.com>
2017-12-19 10:11:33 -0800

    Add modelApiById function to dry up reducer code

---
+++ src/api/reducers/index.ts
@@ -0,0 +1,31 @@
+import { Action } from "redux";
+import { IApiResponse, IApiUpdateResponse, IModel, ModelApi } from "../actions/index";
+import { IModelById } from "../model/index";
+
+export function modelApiById<T extends IModel>(Api: ModelApi<T>) {
+  return (state: IModelById<T> = {}, action: Action): IModelById<T> => {
+    switch (action.type) {
+      case Api.SUCCESSFUL_GET_ALL_TYPE: {
+        const objects = (action as IApiResponse<T[]>).payload;
+        const newState = {...state};
+        objects.forEach((object) => {
+          newState[object.id] = object;
+        });
+      }
+      case Api.SUCCESSFUL_CREATE_TYPE: {
+        const object = (action as IApiResponse<T>).payload;
+        const newState = {...state};
+        newState[object.id] = object;
+        return newState;
+      }
+      case Api.SUCCESSFUL_UPDATE_TYPE: {
+        const object = (action as IApiUpdateResponse<T>).updated;
+        const newState = {...state};
+        newState[object.id] = object;
+        return newState;
+      }
+      default:
+        return state;
+    }
+  };
+}
