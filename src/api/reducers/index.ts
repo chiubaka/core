50b072d7b32cbad7efe78158619646e19c2a2168
Daniel Chiu <daniel@chiubaka.com>
2017-12-19 13:13:31 -0800

    Add a second API model reducer factory for simple array state representations

--- src/api/reducers/index.ts
+++ src/api/reducers/index.ts
@@ -30,3 +30,36 @@ export function modelApiById<T extends IModel>(Api: ModelApi<T>) {
     }
   };
 }
+
+// Manages API state as a simple array. Useful for simple models that have relatively small
+// amounts of data which does not need to be replicated or sliced and displayed elsewhere.
+export function modelApiAsArray<T extends IModel>(Api: ModelApi<T>) {
+  return (state: T[] = [], action: Action): T[] => {
+    switch (action.type) {
+      case Api.SUCCESSFUL_GET_ALL_TYPE: {
+        return (action as IApiResponse<T[]>).payload;
+      }
+      case Api.SUCCESSFUL_CREATE_TYPE: {
+        const object = (action as IApiResponse<T>).payload;
+        return [...state, object];
+      }
+      case Api.SUCCESSFUL_UPDATE_TYPE: {
+        const updated = (action as IApiUpdateResponse<T>).updated;
+        const index = state.findIndex((object) => {
+          return object.id === updated.id;
+        });
+
+        if (index === -1) {
+          console.warn("API attempt to update an object which does not exist on client.");
+          return [...state, updated];
+        }
+
+        const newState = [...state];
+        newState.splice(index, 1, updated);
+        return newState;
+      }
+      default:
+        return state;
+    }
+  }
+}
