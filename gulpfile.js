607cd479664c1981dd425756dde26f7db77162ed
Daniel Chiu <daniel@chiubaka.com>
2018-02-14 16:31:46 +0700

    Compile dist down to ES5 for IE11 support

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+const babel = require('gulp-babel');
+const merge = require('merge2');
+
+const tsProject = ts.createProject('./tsconfig.json');
+
+gulp.task('default', () => {
+  const tsResult =  gulp.src('src/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+
+  return merge([
+    tsResult.js
+      .pipe(babel({
+        presets: ['es2015'],
+        plugins: ['transform-object-assign'],
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist')),
+    tsResult.dts.pipe(gulp.dest('dist')),
+  ]);
+});
