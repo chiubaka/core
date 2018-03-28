aa5f4b979cbf94d9e3c35e39c3005e0bea88cca2
Daniel Chiu <daniel@chiubaka.com>
2018-03-28 14:08:31 +0700

    Remove unused variables from Jumbotron.tsx

--- src/marketing/components/Jumbotron/Jumbotron.tsx
+++ src/marketing/components/Jumbotron/Jumbotron.tsx
@@ -2,16 +2,12 @@ import * as classnames from "classnames";
 import * as React from "react";
 
 export interface IJumbotronProps {
-  backgroundColor?: string;
-  backgroundImage?: string;
   className?: string;
-  overlayOpacity?: number;
 }
 
 export class Jumbotron extends React.Component<IJumbotronProps> {
   public render(): JSX.Element {
-    const {backgroundColor, backgroundImage, className} = this.props;
-    const overlayOpacity = this.props.overlayOpacity ? this.props.overlayOpacity : 0;
+    const {className} = this.props;
 
     return (
       <div
