ddc29c551b6dae80f537392c0a0e15854e56a932
Daniel Chiu <daniel@chiubaka.com>
2018-03-29 18:17:37 +0700

    Add optional transformers for modelApi reducers

--- src/api/reducers/index.ts
+++ src/api/reducers/index.ts
@@ -2,11 +2,14 @@ import { Action } from "redux";
 import { IApiResponse, IApiUpdateResponse, IModel, ModelApi } from "../actions/index";
 import { IModelById } from "../model/index";
 
-export function modelApiById<T extends IModel>(Api: ModelApi<T>) {
+export declare type ReducerTransformer<T> = (item: T) => T;
+
+export function modelApiById<T extends IModel>(Api: ModelApi<T>, transformer?: ReducerTransformer<T>) {
   return (state: IModelById<T> = {}, action: Action): IModelById<T> => {
     switch (action.type) {
       case Api.SUCCESSFUL_GET_ALL_TYPE: {
-        const objects = (action as IApiResponse<T[]>).payload;
+        const payload = (action as IApiResponse<T[]>).payload;
+        const objects = transformer ? payload.map(transformer) : payload;
         const newState = {...state};
         objects.forEach((object) => {
           newState[object.id] = object;
@@ -21,7 +24,8 @@ export function modelApiById<T extends IModel>(Api: ModelApi<T>) {
       }
       case Api.SUCCESSFUL_GET_TYPE:
       case Api.SUCCESSFUL_UPDATE_TYPE: {
-        const object = (action as IApiUpdateResponse<T>).payload;
+        const payload = (action as IApiUpdateResponse<T>).payload;
+        const object = transformer ? transformer(payload) : payload;
         const newState = {...state};
         newState[object.id] = object;
         return newState;
@@ -34,11 +38,12 @@ export function modelApiById<T extends IModel>(Api: ModelApi<T>) {
 
 // Manages API state as a simple array. Useful for simple models that have relatively small
 // amounts of data which does not need to be replicated or sliced and displayed elsewhere.
-export function modelApiAsArray<T extends IModel>(Api: ModelApi<T>) {
+export function modelApiAsArray<T extends IModel>(Api: ModelApi<T>, transformer?: ReducerTransformer<T>) {
   return (state: T[] = [], action: Action): T[] => {
     switch (action.type) {
       case Api.SUCCESSFUL_GET_ALL_TYPE: {
-        return (action as IApiResponse<T[]>).payload;
+        const payload = (action as IApiResponse<T[]>).payload;
+        return transformer ? payload.map(transformer) : payload;
       }
       case Api.SUCCESSFUL_CREATE_TYPE: {
         const object = (action as IApiResponse<T>).payload;
@@ -46,7 +51,8 @@ export function modelApiAsArray<T extends IModel>(Api: ModelApi<T>) {
       }
       case Api.SUCCESSFUL_GET_TYPE:
       case Api.SUCCESSFUL_UPDATE_TYPE: {
-        const updated = (action as IApiUpdateResponse<T>).payload;
+        const payload = (action as IApiUpdateResponse<T>).payload;
+        const updated = transformer ? transformer(payload) : payload;
         const index = state.findIndex((object) => {
           return object.id === updated.id;
         });
