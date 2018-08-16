a1e329602e7b51b5b50158547c796b5ed064bc12
Daniel Chiu <daniel@chiubaka.com>
2018-08-16 15:53:48 +0200

    Handle pulling JWT token from auth header and receiving user details with token

--- src/api/actions/Api.ts
+++ src/api/actions/Api.ts
@@ -110,6 +110,29 @@ export class Api {
     return this.requestWithPayload(pathname, payload, "PUT", dispatch, token);
   }
 
+  protected handleApiResponse<T>(dispatch: Dispatch<IAuthState>, response: Response): Promise<T | string> {
+    switch (response.status) {
+      case HttpStatus.OK:
+      case HttpStatus.CREATED:
+        console.log("Successful response");
+        return response.json();
+      case HttpStatus.UNAUTHORIZED:
+        dispatch(completeLogout());
+        dispatch(push("/auth/login", {
+          // TODO: This path should be configurable and not constant.
+          redirectPath: "/app",
+        }));
+        return Promise.reject("You are not logged in.");
+      case HttpStatus.BAD_REQUEST:
+        return response.json().then(this.errorTransformer.bind(this, response.url));
+      case HttpStatus.INTERNAL_SERVER_ERROR:
+      case HttpStatus.GATEWAY_TIMEOUT:
+        return Promise.reject("An unexpected error has occurred. Please try again later.");
+      default:
+        return Promise.reject(`Received unexpected status code ${response.status}`);
+    }
+  }
+
   private requestWithPayload<T>(pathname: string, payload: T, method: "POST" | "PUT",
                                 dispatch: Dispatch<IAuthState>, token: string) {
     return this.request(pathname, dispatch, token, {
@@ -131,26 +154,5 @@ export class Api {
       this.handleUnsuccessfulRequest(reason, dispatch);
       return Promise.reject(reason);
     });
-  }
-
-  private handleApiResponse<T>(dispatch: Dispatch<IAuthState>, response: Response): Promise<T | string> {
-    switch (response.status) {
-      case HttpStatus.OK:
-      case HttpStatus.CREATED:
-        return response.json();
-      case HttpStatus.UNAUTHORIZED:
-        dispatch(completeLogout());
-        dispatch(push("/auth/login", {
-          redirectPath: "/app",
-        }));
-        return Promise.reject("You are not logged in.");
-      case HttpStatus.BAD_REQUEST:
-        return response.json().then(this.errorTransformer.bind(this, response.url));
-      case HttpStatus.INTERNAL_SERVER_ERROR:
-      case HttpStatus.GATEWAY_TIMEOUT:
-        return Promise.reject("An unexpected error has occurred. Please try again later.");
-      default:
-        return Promise.reject(`Received unexpected status code ${response.status}`);
-    }
   }
 }
