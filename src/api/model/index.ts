118092de07c5c695935ef4f7f630395cd9c299f5
Daniel Chiu <daniel@chiubaka.com>
2018-09-01 01:01:07 +0100

    Add reducers for indexing on arbitrary model properties

--- src/api/model/index.ts
+++ src/api/model/index.ts
@@ -1,7 +1,12 @@
-export interface IModelById<T> {
+export interface IModel {
+  [propertyName: string]: any;
+  id?: string;
+}
+
+export interface IModelById<T extends IModel> {
   [id: string]: T;
 }
 
-export interface IModel {
-  id?: string;
+export interface IModelIndex<T extends IModel> {
+  [key: string]: T[];
 }
