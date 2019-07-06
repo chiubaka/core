96a8d56a21406cb62ecdc2956f55870e96e07c1b
Daniel Chiu <daniel@chiubaka.com>
2019-07-05 20:54:38 -0500

    Change auth reducer type to satisfy updated Redux typings

--- dist/auth/reducers/index.d.ts
+++ dist/auth/reducers/index.d.ts
@@ -1,3 +1,3 @@
 import { Action } from "redux";
 import { IAuthInnerState } from "../model/AuthenticationState";
-export declare function auth(state: IAuthInnerState, action: Action): IAuthInnerState;
+export declare function auth(state: IAuthInnerState | undefined, action: Action): IAuthInnerState;
