ba95c495ea2ba6973dd7338a2ae4afefc02773a3
Daniel Chiu <daniel@chiubaka.com>
2019-07-10 11:38:20 -0500

    Add utility function for generating UUIDs

--- dist/api/model/index.d.ts
+++ dist/api/model/index.d.ts
@@ -1,2 +1,3 @@
 export * from "./Model";
 export * from "./types";
+export declare function generateId(): string;
--- src/api/model/index.ts
+++ src/api/model/index.ts
@@ -1,2 +1,8 @@
+import * as uuid from "uuid";
+
 export * from "./Model";
 export * from "./types";
+
+export function generateId(): string {
+  return uuid.v4();
+}
