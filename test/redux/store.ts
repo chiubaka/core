22d608444d4bb714ebcc15b609929afb43f98241
Daniel Chiu <daniel@chiubaka.com>
2019-07-28 23:25:39 -0500

    Add tests for ModelApi

--- test/redux/store.ts
+++ test/redux/store.ts
@@ -1,6 +1,13 @@
 import configureStore from "redux-mock-store";
+import { FlushThunks } from "redux-testkit";
 import thunk from "redux-thunk";
 
-const middlewares = [thunk];
+export const flushThunks = FlushThunks.createMiddleware();
+
+const middlewares = [flushThunks, thunk];
 const mockStore = configureStore(middlewares);
-export const store = mockStore({});
+export const store = mockStore({
+  auth: {
+    token: "foobar",
+  },
+});
