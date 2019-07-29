1b281c72c839f35088b236080e1f4c46e493d6fb
Daniel Chiu <daniel@chiubaka.com>
2019-07-28 20:36:35 -0500

    Add tests for RestClient

---
+++ test/redux/store.ts
@@ -0,0 +1,6 @@
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+export const store = mockStore({});
---
+++ test/setup/setupFetchMock.ts
@@ -0,0 +1,31 @@
+
+import fetchMock, { FetchMockSandbox, FetchMockStatic} from "fetch-mock";
+import "isomorphic-fetch";
+import Global = NodeJS.Global;
+
+export interface IGlobalWithFetchMock extends Global {
+  fetchMock: FetchMockStatic;
+  fetch: FetchMockSandbox;
+  Headers: any;
+}
+
+fetchMock.config.overwriteRoutes = false;
+
+const customGlobal: IGlobalWithFetchMock = global as IGlobalWithFetchMock;
+customGlobal.fetch = fetchMock.sandbox();
+customGlobal.fetchMock = fetchMock;
+customGlobal.Headers = class Headers {
+  public headers: {[key: string]: string};
+
+  constructor() {
+    this.headers = {};
+  }
+
+  public append(key, value) {
+    this.headers[key] = value;
+  }
+
+  public get(key) {
+    return this.headers[key];
+  }
+};
