08566d9f984baa142d789de8510a16178c2f81a8
Daniel Chiu <daniel@chiubaka.com>
2019-08-03 17:02:22 -0500

    Add a couple generalized selectors for grabbing models

--- src/orm/selectors/index.ts
+++ src/orm/selectors/index.ts
@@ -2,6 +2,19 @@ import { createSelector as createOrmSelector, ORM } from "redux-orm";
 
 import { Model } from "../model";
 
+export const modelIdsSelector = (orm: ORM, model: typeof Model, sortFn: (a: any, b: any) => number = null) => {
+  return createOrmSelector(
+    orm,
+    (session) => {
+      const instances = session[model.modelName].all().toModelArray();
+      if (sortFn != null) {
+        instances.sort(sortFn);
+      }
+      return instances.map((instance) => instance.id);
+    },
+  );
+};
+
 export const modelSelector = (orm: ORM, model: typeof Model, id: string) => {
   return createOrmSelector(
     orm,
